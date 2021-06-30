15028ae0096cbef3f0818f4381857bbd9ac5d742
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2021-06-30 20:27:37 +0300

    change method order

--- src/A.java
+++ src/A.java
@@ -3,11 +3,11 @@
  * Date: 20/07/2018
  */
 public class A {
-    void methodA() {
-        System.out.println("A");
-    }
-
     void methodB() {
         System.out.println("B");
+    }
+
+    void methodA() {
+        System.out.println("A");
     }
 }
