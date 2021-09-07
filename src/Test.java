8f2bfdeb64718b354037150ec7256da2fdcb7078
Dmitry Zhuravlev <dmitry.zhuravlev@jetbrains.com>
2021-09-07 22:48:00 +0300

    optimize back

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.color.ColorSpace;
 import com.sun.deploy.util.StringUtils;
+import com.sun.deploy.util.ArrayUtil;
 
 public class Test {
     Rectangle r;
     StringUtils u;
+    ArrayUtil a;
 }
